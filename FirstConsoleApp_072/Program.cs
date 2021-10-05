e68abd970d20a80b3cb7c495c756f0a02a3a35e3
anisanurfadila <anisanfd925@gmail.com>
2021-10-05 20:24:56 +0700

    Add project files.

---
+++ FirstConsoleApp_072/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FirstConsoleApp_072
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
